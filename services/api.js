2036ac119e3cea7711382e474dff94a530168170
Nabil Zafari <132698986+zafarinabil@users.noreply.github.com>
2024-01-16 11:11:34 +0100

    env file updated

--- services/api.js
+++ services/api.js
@@ -1,14 +1,17 @@
-import { MongoExpiredSessionError } from "mongodb";
-import fetch from "node-fetch";
+import fetch from 'node-fetch';
 
+// Hämtar alla produkter från Fake store api
+// OBS! Se till att ge FAKE_STORE_API_URL url:en i din .env fil
 export async function getProducts() {
-    const response = await fetch(`${process.env.FAKE_STORE_API_URL}/products`);
-    const products = await response.json();
-    return products;
+	const res = await fetch(`${process.env.FAKE_STORE_API_URL}/products`);
+	const products = await res.json();
+	return products;
 }
 
+// Hämtar en produkt från Fake store api
+// OBS! Se till att ge FAKE_STORE_API_URL url:en i din .env fil
 export async function getProduct(id) {
-    const response = await fetch(`${process.env.FAKE_STORE_API_URL}/products/${id}`);
-    const product = await response.json();
-    return product;  
+	const res = await fetch(`${process.env.FAKE_STORE_API_URL}/products/${id}`);
+	const product = await res.json();
+	return product;
 }
