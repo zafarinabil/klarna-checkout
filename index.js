6c14e50f83d17c5ccd2bbecf8f9bf2ee10d3d01d
Nabil Zafari <132698986+zafarinabil@users.noreply.github.com>
2024-01-17 17:39:46 +0100

    kris-update

--- index.js
+++ index.js
@@ -1,37 +1,45 @@
-import { getProducts, getProduct } from "./services/api.js";
-import { createOrder, retrieveOrder } from "./services/klarna.js";
-import express, { response } from "express";
+import { getProducts, getProduct } from './services/api.js';
+import { createOrder, retrieveOrder } from './services/klarna.js';
+import express from 'express';
 const app = express();
-import { config } from "dotenv";
+import { config } from 'dotenv';
 config();
 
-console.log(process.env.BASE_URL);
-
-app.get('/', async (request, response) => {
-    const products = await getProducts();
-    // console.log(products);
-    const markup = products.map((p)=>`<a style="display: block; color: black; border: solid 2px black; margin: 20px; padding: 10px;" href="/products/${p.id}"> ${p.title} <br> ${p.price} SEK<a/>`).join(" ");
-    response.send(markup);
+app.get('/', async (req, res) => {
+	const products = await getProducts();
+	const markup = products
+		.map(
+			(p) =>
+				`<div style="display: flex; flex-direction: column; justify-content: space-between; align-items: center; flex: 0 0 calc(200px - 20px); margin: 10px; padding: 15px; border: 1px solid #ccc; text-align: center;">
+					<p style="font-size: 16px; font-weight: bold; padding: 0; margin: 0;">${p.title}</p>
+					<div style="display: flex; flex-direction: column; justify-content: center; align-items: center;">
+						<p style="font-size: 14px; color: #888; padding: 0; margin: 10px 0 0 0;">${p.price} kr</p>
+						<button style="border: 1px solid black; border-radius: 6px; transition: border-radius 150ms; width: 64px; padding: 8px 0; background-color: white; font-size: 14px; color: black; cursor: pointer; margin-top: 10px;" onmouseover="this.style.borderRadius='10px'" onmouseout="this.style.borderRadius='6px'" onclick="window.location.href='/product/${p.id}'">Köp</button>
+					</div>
+				</div>`
+		)
+		.join(' ');
+	const wrapperMarkup = `<div style="display: flex; flex-direction: row; flex-wrap: wrap; justify-content: center;">${markup}</div>`;
+	res.send(wrapperMarkup);
 });
 
-
-app.get('/products/:id', async (request, response) => {
-    try {
-        const { id } = request.params;
-        const product = await getProduct(id);
-        const klarnaResponse =  await createOrder(product);
-        const markup = klarnaResponse.html_snippet;
-        response.send(markup);
-    } catch (error) {
-        response.send(error.message)
-    }
+app.get('/product/:id', async function (req, res) {
+	try {
+		const { id } = req.params;
+		const product = await getProduct(id);
+		const klarnaJsonResponse = await createOrder(product);
+		const html_snippet = klarnaJsonResponse.html_snippet;
+		res.send(html_snippet);
+	} catch (error) {
+		res.send(error.message);
+	}
 });
 
-app.get('/confirmation', async (request, response) => {
-    const { order_id } = request.query;
-    const klarnaResponse = await retrieveOrder(order_id);
-    const  { html_snippet } = klarnaResponse; 
-    response.send(html_snippet);
+app.get('/confirmation', async function (req, res) {
+	const order_id = req.query.order_id;
+	const klarnaJsonResponse = await retrieveOrder(order_id);
+	const html_snippet = klarnaJsonResponse.html_snippet;
+	res.send(html_snippet);
 });
 
 app.listen(process.env.PORT);
--- services/klarna.js
+++ services/klarna.js
@@ -1,88 +1,89 @@
-import fetch from "node-fetch";
+import fetch from 'node-fetch';
 
 export function getKlarnaAuth() {
-    const username = process.env.PUBLIC_KEY;
-    const password = process.env.SECRET_KEY;
-    const auth = `Basic ${Buffer.from(username + ':' + password).toString('base64')}`;
-    return auth;
+	const username = process.env.PUBLIC_KEY;
+	const password = process.env.SECRET_KEY;
+	// const auth = `Basic ${Buffer.from(username + ':' + password).toString('base64')}`;
+	const auth = 'Basic ' + Buffer.from(username + ':' + password).toString('base64');
+	return auth;
 }
 
-//Creating order at Klarna
+// Create a order at Klarna
 export async function createOrder(product) {
-    const path = '/checkout/v3/orders';
-    const auth = getKlarnaAuth();
-    const url = process.env.BASE_URL + path;
-    const method = 'POST';
-    const headers = {
-        'Content-Type' : 'application/json',
-        Authorization : auth
-    };
-    const quantity = 1;
-    const price = product.price * 100; 
-    const total_amount = price * quantity;
-    const total_tax_amount = total_amount * 0.2;
+	const path = '/checkout/v3/orders';
+	const auth = getKlarnaAuth();
 
-    const payload = {
-        purchase_country: 'SE',
-        purchase_currency: 'SEK',
-        locale: 'sv-SE',
-        order_amount: total_amount,
-        order_tax_amount: total_tax_amount,
-        order_lines: [
-            {
-                type: 'physical',
-                refrence: product.id,
-                name: product.title,
-                quantity,
-                quantity_unit: 'pcs',
-                unit_price: price,
-                tax_rate: 2500,
-                total_amount,
-                total_discount_amount: 0,
-                total_tax_amount,
-                image_url: product.image
-            }
-        ],
-        merchant_urls : {
-            terms: 'https://www.example.com/terms.html',
-            checkout: 'https://www.example.com/checkout.html',
-            confirmation: `${process.env.CONFIRMATION_URL}?order_id={checkout.order.id}`,
-            push: 'https://www.example.com/api/push'
-            //nabil
-        }
-    };
-    const body = JSON.stringify(payload);
-    const response = await fetch(url, {method, headers, body});
-    const json = await response.json();
+	const url = process.env.BASE_URL + path;
+	const method = 'POST';
+	const headers = {
+		'Content-Type': 'application/json',
+		Authorization: auth
+	};
 
-    if(response.status === 200 ||  response.status === 201) {
-        return json;
-    } else {
-        console.error('Error ', json);
-        return {
-            html_snippet: `<h1>${JSON.stringify(json)}</h1>`
-        };
-    }
+	const quantity = 1;
+	const price = product.price * 100;
+	const total_amount = price * quantity;
+	const total_tax_amount = total_amount * 0.2;
 
+	const payload = {
+		purchase_country: 'SE',
+		purchase_currency: 'SEK',
+		locale: 'sv-SE',
+		order_amount: total_amount,
+		order_tax_amount: total_tax_amount,
+		order_lines: [
+			{
+				type: 'physical',
+				reference: product.id,
+				name: product.title,
+				quantity,
+				quantity_unit: 'pcs',
+				unit_price: price,
+				tax_rate: 2500,
+				total_amount,
+				total_discount_amount: 0,
+				total_tax_amount
+			}
+		],
+		merchant_urls: {
+			terms: 'https://www.example.com/terms.html',
+			checkout: 'https://www.example.com/checkout.html?order_id={checkout.order.id}',
+			confirmation: `${process.env.CONFIRMATION_URL}?order_id={checkout.order.id}`,
+			push: 'https://www.example.com/api/push?order_id={checkout.order.id}'
+		}
+	};
+
+	const body = JSON.stringify(payload);
+	const response = await fetch(url, { method, headers, body });
+	const jsonResponse = await response.json();
+
+	if (response.status === 200 || response.status === 201) {
+		return jsonResponse;
+	} else {
+		console.error('ERROR: ', jsonResponse);
+		return {
+			html_snippet: `<p>${JSON.stringify(jsonResponse)}</p>`
+		};
+	}
 }
 
-//Catch an order from Klarna
+// Retrieve a order at Klarna
 export async function retrieveOrder(order_id) {
-    const path = `/checkout/v3/orders/${order_id}`;
-    const auth = getKlarnaAuth();
-    const url = process.env.BASE_URL + path;
-    const method = 'GET';
-    const headers = { Authorization: auth };
-    const response = await fetch(url, {method, headers});
+	const path = '/checkout/v3/orders/' + order_id;
+	const auth = getKlarnaAuth();
 
-    if(response.status === 200 || response.status === 201) {
-        const json = await response.json();
-        return json;
-    } else {
-        return {
-            html_snippet: `<h1>${response.status} ${response.statusText} </h1>`
-        }
-    }
+	const url = process.env.BASE_URL + path;
+	const method = 'GET';
+	const headers = { Authorization: auth };
+	const response = await fetch(url, { method, headers });
 
+	if (response.status === 200 || response.status === 201) {
+		const jsonResponse = await response.json();
+		return jsonResponse;
+	} else {
+		console.error('ERROR: ', response.status, response.statusText);
+		return {
+			html_snippet: `<p>${response.status} ${response.statusText}</p>`
+		};
+	}
 }
-
